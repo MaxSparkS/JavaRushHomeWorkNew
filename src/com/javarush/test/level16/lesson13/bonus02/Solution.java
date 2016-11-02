4597e1f8649493b5d1dcabf211b7ff85fcf77191
Max <5611771@mail.ru>
2016-11-02 13:57:22 +0300

    11/1/2016

--- src/com/javarush/test/level16/lesson13/bonus02/Solution.java
+++ src/com/javarush/test/level16/lesson13/bonus02/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level16.lesson13.bonus02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,50 +19,162 @@ import java.util.List;
 */
 
 public class Solution {
-    public static List<Thread> threads = new ArrayList<Thread>(5);
-    static {
-        threads.add(new ThreadOne());
-        threads.add(new ThreadTwo());
-        threads.add(new ThreadThree());
-        threads.add(new ThreadFour());
-        threads.add(new ThreadFive());
+public static List<Thread> threads = new ArrayList<Thread>(5);
+
+static {
+
+    threads.add(new ThreadOne());
+    threads.add(new ThreadTwo());
+    threads.add(new ThreadThree());
+    threads.add(new ThreadFour());
+    threads.add(new ThreadFive());
+}
+
+private static void sleep()
+{
+    try
+    {
+        Thread.sleep(100);
     }
+    catch (InterruptedException e)
+    {
+        e.printStackTrace();
+    }
+}
 
 
-    public static class ThreadOne extends Thread{
-    @Override
-    public void run() {
-        while (true){}
-    }
-    }
+public static void main(String[] args){
 
-    public static class ThreadTwo extends Thread{
-    @Override
-    public void run() {
-        try {
-            while (!isInterrupted()){}
-            throw new InterruptedException();
-        }catch (InterruptedException e){
-            System.out.println("InterruptedException");
+    Thread thread2 = threads.get(1);
+    thread2.start();
+    sleep();
+    thread2.interrupt();
+
+    Thread thread4 = threads.get(3);
+    Message message = (Message) thread4;
+    thread4.start();
+    sleep();
+    message.showWarning();
+    System.out.println(thread4.isAlive());
+
+}
+
+public static class ThreadOne extends Thread{
+
+@Override
+public void run()
+{
+    while (true)
+    {
+
+    }
+}
+}
+
+public static class ThreadTwo extends Thread{
+
+@Override
+public void run()
+{
+    try{
+        while (!isInterrupted())
+        {
+
         }
+        throw new InterruptedException();
+    }catch (InterruptedException e)
+    {
+        System.out.println("InterruptedException");
     }
+
+}
+}
+
+public static class ThreadThree extends Thread{
+
+@Override
+public void run()
+{
+    try{
+        while (true)
+        {
+            System.out.println("Ура");
+            Thread.sleep(500);
+        }
+
+    }catch (InterruptedException e)
+    {
+
     }
-    public static class ThreadThree extends Thread{
-    @Override
-    public void run() {
-        try{
-            while (true){
-                System.out.println("Ура");
-                Thread.sleep(500);
+}
+}
+
+public static class ThreadFour extends Thread implements Message{
+
+@Override
+public  void showWarning()
+{
+
+    this.interrupt();
+    try
+    {
+        this.join();
+    }
+    catch(Exception e)
+    {
+
+    }
+
+}
+
+@Override
+public void run()
+{
+    Thread current = Thread.currentThread();
+    while(!current.interrupted())
+    {
+
+    }
+
+
+}
+}
+
+public static class ThreadFive extends Thread {
+
+@Override
+public void run()
+{
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int sum = 0;
+
+    try{
+
+        while (!isInterrupted())
+        {
+            String s = reader.readLine();
+
+            if (s.equals("N")) this.interrupt();
+
+            else
+            {
+                int integer = Integer.parseInt(s);
+
+                sum += integer;
             }
-        }catch (InterruptedException e){}
-    }
-    }
-    public static class ThreadFour extends Thread{
+        }
 
-    }
-    public static class ThreadFive extends Thread{
+        throw new InterruptedException();
 
+    }catch (IOException e)
+    {
+
+    }catch (InterruptedException e)
+    {
+        System.out.println(sum);
     }
 
+
+}
+}
 }
