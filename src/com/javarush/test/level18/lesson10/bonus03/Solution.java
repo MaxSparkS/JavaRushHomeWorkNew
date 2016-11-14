53903c606945ee4b17b6e352931f8277d5a3116a
Max <5611771@mail.ru>
2016-11-14 19:00:57 +0300

    don't work

--- src/com/javarush/test/level18/lesson10/bonus03/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus03/Solution.java
@@ -24,8 +24,54 @@ id productName price quantity
 19847983Куртка для сноубордистов, разм10173.991234
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        BufferedReader file = new BufferedReader(new FileReader(fileName));
+        ArrayList<String> list = new ArrayList<>();
+        String line;
+        while ((line=file.readLine())!=null){
+            list.add(line);
+        }
+//        for (String s : list){
+//            int id;
+//            String productName;
+//            double price;
+//            int quantity;
+//        }
+        if (args.length==5 && args[0].equals("-u")){
+//            try{
+                int id = Integer.parseInt(args[1]);
+                String productName = args[2];
+                double price = Double.parseDouble(args[3]);
+                int quantity = Integer.parseInt(args[4]);
+                for (String s : list){
+                    int id1 = Integer.parseInt(s.substring(0,8).replace(" ", ""));
+                    System.out.println(s.substring(0,8).replace(" ",""));
+                    String name = s.substring(8,38);
+                    System.out.println(name);
+//                    double price1 = Double.parseDouble(s.substring(38,46).replaceAll(" ", ""));
+                    System.out.println(s.substring(38,46).replaceAll(" ", ""));
+//                    int quantity1 = Integer.parseInt(s.substring(46));
+
+//                    System.out.println(id1+name+price1+quantity1);
+                }
+//            }catch (Exception e){
+//                System.out.println("Exception");
+//            }
+        }
+        else if (args.length==2 && args[0].equals("-d")){
+            try{
+                int id = Integer.parseInt(args[1]);
+            }catch (Exception e){}
+        }
     }
 }
