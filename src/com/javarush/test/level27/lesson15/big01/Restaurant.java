70669f3c47b78086d9a1d635902905f9cff080bb
Max <5611771@mail.ru>
2016-11-23 17:23:29 +0300

    project Restaurant - begin

---
+++ src/com/javarush/test/level27/lesson15/big01/Restaurant.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level27.lesson15.big01;
+
+/**
+ * Created by m.shkurko on 11/23/2016.
+ */
+public class Restaurant
+    {
+
+    public static void main(String[] args) {
+
+    }
+    }
